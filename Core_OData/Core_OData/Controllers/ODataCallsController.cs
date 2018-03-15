9236ed1789a90ddb94a896e682dd2ff7cd558bc7
sanjog35 <37402067+sanjog35@users.noreply.github.com>
2018-03-16 01:37:45 +0530

    Add files via upload
    Initial Commit

---
+++ Core_OData/Core_OData/Controllers/ODataCallsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Core_OData.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/ODataCalls")]
+    public class ODataCallsController : Controller
+    {
+        [HttpGet]
+        [Route("OData")]
+        public async Task<IActionResult> GetOData()
+        {
+            string data = null;
+            try
+            {
+                using (var _client = new HttpClient())
+                {
+                    string uri = "http://services.odata.org/V4/TripPinServiceRW";
+
+                    using (HttpResponseMessage _response = await _client.GetAsync(uri))
+                    using (HttpContent _context = _response.Content)
+                    {
+                        data = await _context.ReadAsStringAsync();
+                    }                    
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return new JsonResult(data);
+        }
+    }
+}
---
+++ Core_OData/Core_OData/ODataReturnModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_OData
+{
+    public class ODataReturnModel
+    {
+        public string name { get; set; }
+        public string kind { get; set; }
+        public string url { get; set; }
+    }
+}
